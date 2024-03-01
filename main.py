c72173f5394de71c42d075d258e80f603903a313
_gacut_ <xzibitgacut@gmail.com>
2024-03-01 18:26:13 +0100

    modified:   main.py     Dodanie powitalnego kodu

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-main
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+
+class ZabezpieczDusze(MDApp):
+    def build(self):
+        
+        return MDLabel(text="Witaj 100commitów!", halign="center")
+    
+
+ZabezpieczDusze().run()
