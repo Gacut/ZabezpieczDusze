b510eab576fa05df3b22b924b9f265ffab3bb7ff
_gacut_ <xzibitgacut@gmail.com>
2024-03-01 18:27:28 +0100

    modified:   main.py     Dodanie kodu powitalnego

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivymd.uix.floatlayout import FloatLayout
+
 
 class ZabezpieczDusze(MDApp):
     def build(self):
-        
-        return MDLabel(text="Witaj 100commitów!", halign="center")
+        background = Image(source='grafiki/bg1.jpg', allow_stretch=True, keep_ratio=False)
+        layout = FloatLayout()
+        label = Label(text="Witaj 100commitów!", color=[156, 17, 149, 1])
+        label.halign="right"
+        layout.add_widget(background)
+        layout.add_widget(label)
+
+
+        return layout
     
 
 ZabezpieczDusze().run()
